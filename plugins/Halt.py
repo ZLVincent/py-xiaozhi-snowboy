ee4e9ea6de30fab399ce85252356d80439bf16e2
vincentzhangl <vincent.zhang@mingie.cn>
2025-04-29 20:36:04 +0800

    1

--- plugins/Halt.py
+++ plugins/Halt.py
@@ -3,7 +3,7 @@
 import time
 import subprocess
 from common import logging, utils, constants
-from robot import AbstractPlugin
+from robot.AbstractPlugin import AbstractPlugin
 
 logger = logging.getLogger(__name__)   
 
